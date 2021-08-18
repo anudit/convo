1db2f492910e6308ed62414d9952eb7d27ca9684
Anudit Nagar <nagaranudit@gmail.com>
2021-08-18 16:59:59 +0530

    Start NEAR Auth

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -1,5 +1,17 @@
 import jwt from 'jsonwebtoken';
 import { isAddress, verifyMessage } from 'ethers/lib/utils';
+import nacl from 'tweetnacl';
+const { Crypto } = require("@peculiar/webcrypto");
+
+async function validateNearSignature(data, signature, signerAddress){
+  const tokenMessage = new TextEncoder().encode(data);
+  let sig = new Uint8Array(signature.toString().split(',').map((e)=>{return parseInt(e) }));
+  let sigAdd = new Uint8Array(signerAddress.toString().split(',').map((e)=>{return parseInt(e) }));
+  const webcrypto = new Crypto();
+  const hashed = await webcrypto.subtle.digest('SHA-256', tokenMessage);
+  const hash = new Uint8Array(hashed);
+  return nacl.sign.detached.verify(hash, sig, sigAdd);
+}
 
 export default async (req, res) => {
 
@@ -12,52 +24,115 @@ export default async (req, res) => {
 
   try {
 
-    if (
-      Object.keys(req.body).includes('signature') &&
-      Object.keys(req.body).includes('signerAddress') &&
-      Object.keys(req.body).includes('timestamp') &&
-      isAddress(req.body?.signerAddress)){
+    const chain =  Object.keys(req.body).includes('chain') === true ? req.body?.chain : 'ethereum';
 
-      let currentTimestamp = Date.now();
+    if (chain === 'ethereum') {
+      if (
+        Object.keys(req.body).includes('signature') &&
+        Object.keys(req.body).includes('signerAddress') &&
+        Object.keys(req.body).includes('timestamp') &&
+        isAddress(req.body?.signerAddress)
+      ){
 
-      if (currentTimestamp - req.body.timestamp > 24*60*60*1000){ // stale signature request
-        return res.status(400).json({
-          'success':false,
-          'message': 'Request timestamp too old.'
-        });
-      }
+        let currentTimestamp = Date.now();
 
-      let data = `I allow this site to access my data on The Convo Space using the account ${req.body.signerAddress}. Timestamp:${req.body.timestamp}`;
-      let recoveredAddress = verifyMessage(data, req.body.signature);
+        if (currentTimestamp - req.body.timestamp > 24*60*60*1000){ // stale signature request
+          return res.status(400).json({
+            'success':false,
+            'message': 'Request timestamp too old.'
+          });
+        }
 
-      if(req.body.signerAddress === recoveredAddress){
+        let data = `I allow this site to access my data on The Convo Space using the account ${req.body.signerAddress}. Timestamp:${req.body.timestamp}`;
+        let recoveredAddress = verifyMessage(data, req.body.signature);
 
-        let token = jwt.sign(
-            {user: req.body.signerAddress},
-            process.env.JWT_SECRET,
-            { expiresIn: "1d" }
-        );
+        if(req.body.signerAddress === recoveredAddress){
 
-        return res.status(200).json({
-            'success': true,
-            'message': token
-        });
+          let token = jwt.sign(
+              {user: req.body.signerAddress, chain: "ethereum"},
+              process.env.JWT_SECRET,
+              { expiresIn: "1d" }
+          );
+
+          return res.status(200).json({
+              'success': true,
+              'message': token
+          });
+
+        }
+        else {
+          return res.status(400).json({
+            'success':false,
+            'message': "Recovered address from signature doesn't match signerAddress"
+          });
+        }
 
       }
       else {
         return res.status(400).json({
-          'success':false,
-          'message': "Recovered address from signature doesn't match signerAddress"
+            'success':false,
+            'message': 'signerAddress or signature or timestamp is missing/invalid.'
         });
       }
+
+    }
+    else if(chain  === 'near') {
+
+      if (
+        Object.keys(req.body).includes('signature') &&
+        Object.keys(req.body).includes('signerAddress') &&
+        Object.keys(req.body).includes('accountId') &&
+        Object.keys(req.body).includes('timestamp')
+      ){
+
+        let currentTimestamp = Date.now();
+
+        if (currentTimestamp - req.body.timestamp > 24*60*60*1000){ // stale signature request
+          return res.status(400).json({
+            'success':false,
+            'message': 'Request timestamp too old.'
+          });
+        }
+
+        let data = `I allow this site to access my data on The Convo Space using the account ${req.body.accountId}. Timestamp:${req.body.timestamp}`;
+        let recoverResult = await validateNearSignature(data, req.body.signature, req.body.signerAddress);
+
+        if(recoverResult === true){
+
+          let token = jwt.sign(
+              {user: req.body.accountId, chain: "near"},
+              process.env.JWT_SECRET,
+              { expiresIn: "1d" }
+          );
+
+          return res.status(200).json({
+              'success': true,
+              'message': token
+          });
+
+        }
+        else {
+          return res.status(400).json({
+            'success':false,
+            'message': "Recovered address from signature doesn't match signerAddress"
+          });
+        }
+
+      }
+      else {
+        return res.status(400).json({
+            'success':false,
+            'message': 'signerAddress or signature or timestamp is missing/invalid.'
+        });
+      }
+
     }
     else {
       return res.status(400).json({
-          'success':false,
-          'message': 'signerAddress or signature or timestamp is missing/invalid.'
+        'success': true,
+        'message': "Invalid Chain"
       });
     }
-
 
   } catch (error) {
 
