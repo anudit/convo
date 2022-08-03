d4334c3fe68e6dcbd65013757ddf7722b97ed2d1
Anudit Nagar <nagaranudit@gmail.com>
2022-08-03 14:44:33 +0530

    Update icons.js

--- public/icons.js
+++ public/icons.js
@@ -496,17 +496,6 @@ export const NearIcon = createIcon({
     )
 });
 
-export const UdIcon = createIcon({
-    displayName: 'UdIcon',
-    viewBox: '0 0 58 58',
-    path: (
-        <g>
-            <path d="m64 1v22l-64 26z" fill="#2fe9ff" data-convert-id="common_LogoWithTextIcon_path_2"></path>
-            <path d="m52 0v38c0 11.045695-8.954305 20-20 20s-20-8.954305-20-20v-16l12-6.6v22.6c0 3.8659932 3.1340068 7 7 7s7-3.1340068 7-7v-30.3z" fill="#4c47f7"></path>
-        </g>
-    )
-});
-
 export const DecentralandIcon = createIcon({
     displayName: 'DecentralandIcon',
     viewBox: '0 0 400 400',
